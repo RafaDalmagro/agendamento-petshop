2b2ae54d0fe9e3154f40833dce8236e915ac3a11
Rafael Lima Dalmagro <rafa.dalmagro@hotmail.com>
2025-07-23 11:34:48 -0300

    Configuração inicial das dependencias e pastas.

---
+++ src/main.js
@@ -0,0 +1,18 @@
+"use strict";
+
+// Configuração do Day.js para o idioma português
+import "./lib/dayjs";
+
+//CSS
+
+import "./styles/global.css";
+import "./styles/form.css";
+import "./styles/main.css";
+
+// JS
+
+// import "./modules/form/submit.js";
+// import "./modules/form/date-change.js";
+// import "./modules/schedules/cancel.js";
+
+// import "./modules/page-load.js";
